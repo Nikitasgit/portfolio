4b40a10e1f7899cfa60d3b3d0c277ca8571b6d0c
Victor Leman <victorleman1@gmail.com>
2024-10-27 17:13:39 +0100

    commit

--- src/components/HomeMain.js
+++ src/components/HomeMain.js
@@ -1,7 +1,6 @@
 import React, { useRef } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 import FirstLine from "./lines/FirstLine";
-import bgImage from "../assets/images/pexels-suzyhazelwood-1629236.jpg";
 import { IoLogoHtml5 } from "react-icons/io";
 import { FaCss3Alt, FaNodeJs } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io5";
@@ -21,7 +20,6 @@ const HomeMain = () => {
   return (
     <div ref={containerRef} className="home-main">
       <motion.div className="home-main-container" style={{ opacity }}>
-        <img src={bgImage} alt="" />
         <div className="container-top-items">
           <h1>Victor Leman</h1>
         </div>
@@ -35,26 +33,26 @@ const HomeMain = () => {
           </div>
           <div className="logos">
             <div className="logos-slide">
-              <IoLogoHtml5 className="icons-list-item" />
-              <FaCss3Alt className="icons-list-item" />
-              <IoLogoJavascript className="icons-list-item" />
-              <FaNodeJs className="icons-list-item" />
-              <SiNextdotjs className="icons-list-item" />
-              <SiMongodb className="icons-list-item" />
-              <TbBrandFramerMotion className="icons-list-item" />
-              <SiRedux className="icons-list-item" />
-              <FaSass className="icons-list-item" />
+              <IoLogoHtml5 className="logos-slide-item" />
+              <FaCss3Alt className="logos-slide-item" />
+              <IoLogoJavascript className="logos-slide-item" />
+              <FaNodeJs className="logos-slide-item" />
+              <SiNextdotjs className="logos-slide-item" />
+              <SiMongodb className="logos-slide-item" />
+              <TbBrandFramerMotion className="logos-slide-item" />
+              <SiRedux className="logos-slide-item" />
+              <FaSass className="logos-slide-item" />
             </div>
             <div className="logos-slide">
-              <IoLogoHtml5 className="icons-list-item" />
-              <FaCss3Alt className="icons-list-item" />
-              <IoLogoJavascript className="icons-list-item" />
-              <FaNodeJs className="icons-list-item" />
-              <SiNextdotjs className="icons-list-item" />
-              <SiMongodb className="icons-list-item" />
-              <TbBrandFramerMotion className="icons-list-item" />
-              <SiRedux className="icons-list-item" />
-              <FaSass className="icons-list-item" />
+              <IoLogoHtml5 className="logos-slide-item" />
+              <FaCss3Alt className="logos-slide-item" />
+              <IoLogoJavascript className="logos-slide-item" />
+              <FaNodeJs className="logos-slide-item" />
+              <SiNextdotjs className="logos-slide-item" />
+              <SiMongodb className="logos-slide-item" />
+              <TbBrandFramerMotion className="logos-slide-item" />
+              <SiRedux className="logos-slide-item" />
+              <FaSass className="logos-slide-item" />
             </div>
           </div>
         </div>
